d25a27e110cfce08635061c33cbe2aa5776eaca1
Thiniraxz <thiniragenuka@gmail.com>
2022-04-23 14:40:51 +0530

    Create Register path in router

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "../views/login/Login";
+import RegisterUser from "./views/registration/RegisterUser";
 
 export function Router() {
   return (
@@ -8,6 +9,7 @@ export function Router() {
       <Routes>
         
         <Route path="/login" element={<Login />} />
+        <Route path="/registerUser" element ={<RegisterUser/>}></Route>
       </Routes>
     </BrowserRouter>
   );
