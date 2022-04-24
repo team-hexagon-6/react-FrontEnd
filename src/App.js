fb1d9cbfc1a3ca7e625c2789fa950a5324b9731f
Vinojithab17 <gunaratne.19@cse.mrt.ac.lk>
2022-04-24 23:43:00 +0530

    clerk page implement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from "./logo.svg";
 import "./assets/normalize.css";
 import "./App.css";
+import Clerkpage from "./components/Clerkpage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clerkpage/>
     </div>
   );
 }
