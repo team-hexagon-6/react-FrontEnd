223674fc76152ec1257fce260747161bb23c29c9
Thiniraxz <thiniragenuka@gmail.com>
2022-05-23 10:24:16 +0530

    Validate Add Patient

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AddPatient from "../views/addPatient/AddPatient";
 import Login from "../views/login/Login";
 import NewTest from "../views/new_test/NewTest";
 import RegisterUser from "../views/registration/RegisterUser";
@@ -15,11 +16,10 @@ const  Router =() =>{
         <Route path="/registerUser" element ={<RegisterUser/>}></Route>
         <Route path="/userCompleteRegistration" element ={<UserCompleteRegistration/>}></Route>
         <Route path="/newTest" element = {<NewTest/>}></Route>
+        <Route path="/addPatient" element={<AddPatient/>}></Route>
       </Routes>
     </BrowserRouter>
-    // <div>
-    //   hello world
-    // </div>
+
   );
 }
 
--- src/views/addPatient/AddPatient.jsx
+++ src/views/addPatient/AddPatient.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderOne from "../../components/headers/HeaderOne";
 import { Link } from "react-router-dom";
 import AuthServices from "../../services/AuthServices";
+import "./AddPatient.css";
+import Validation from "../../Validation";
 
 function AddPatient() {
-  const [id, setID] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [nic, setNIC] = useState("");
-  const [contactNumber, setContactNumber] = useState("");
-  const [birthday, setBirthday] = useState("");
+  const formValues = {
+    "First Name": "",
+    "Last Name": "",
+    NIC: "",
+    "Contact Number": "",
+    Email: "",
+    Birthday: "",
+  };
+  const [state, setState] = useState(formValues);
+  const [errorData, setErrorData] = useState(formValues);
+  const [gender, setGender] = useState("Select gender");
+  const [genderError, setGenderError] = useState("");
+  console.log("Gender is ", gender);
+  console.log("Gender Error is ", genderError);
+  const errors = {};
+  const handleValidity = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { value, error } = Validation.addPatient(state);
+    console.log(state);
+    if (gender !== "Male" || gender !== "Female") {
+      setGenderError("Select Gender");
+    }
+    if (error) {
+      error.details.map((item) => {
+        errors[item.path[0]] = item.message;
+      });
+    }
+    setErrorData(errors);
+  };
 
   return (
     <div>
@@ -18,7 +49,7 @@ function AddPatient() {
         <h3 className="header">Add Patient</h3>
         <div className="form">
           <div className="justify-content-center row g-3 align-items-center">
-            <div className="col-auto">
+            <div className="col-3 text-center">
               <label htmlFor="" className="col-form-label">
                 First Name
               </label>
@@ -27,47 +58,166 @@ function AddPatient() {
               <input
                 type="text"
                 className="form-control"
+                name="First Name"
                 aria-describedby="passwordHelpInline"
-                value={firstName}
-                onChange={(e) => {
-                  setFirstName(e.target.value);
-                }}
+                onChange={handleValidity}
               />
             </div>
           </div>
-          {usernameError && (
+          {errorData["First Name"] !== "" && (
             <p
               className="d-flex justify-content-center"
               style={{ color: "red" }}
             >
-              {usernameError}
+              {errorData["First Name"]}
             </p>
           )}
           <div className="justify-content-center row g-3 align-items-center">
-            <div className="col-auto">
+            <div className="col-3 text-center">
               <label htmlFor="" className="col-form-label">
-                Password
+                Last Name
               </label>
             </div>
             <div className="col-auto">
               <input
-                type="password"
-                id="inputPassword"
+                type="text"
+                name="Last Name"
                 className="form-control"
                 aria-describedby="passwordHelpInline"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
+                onChange={handleValidity}
               />
             </div>
           </div>
-          {passwordError && (
+          {errorData["Last Name"] !== "" && (
             <p
               className="d-flex justify-content-center"
               style={{ color: "red" }}
             >
-              {passwordError}
+              {errorData["Last Name"]}
+            </p>
+          )}
+          <div className="justify-content-center row g-3 align-items-center">
+            <div className="col-3 text-center ">
+              <label htmlFor="" className="col-form-label">
+                NIC
+              </label>
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                name="NIC"
+                className="form-control"
+                aria-describedby="passwordHelpInline"
+                onChange={handleValidity}
+              />
+            </div>
+          </div>
+          {errorData["NIC"] !== "" && (
+            <p
+              className="d-flex justify-content-center"
+              style={{ color: "red" }}
+            >
+              {errorData["NIC"]}
+            </p>
+          )}
+          <div className="justify-content-center row g-3 align-items-center">
+            <div className="col-3 text-center">
+              <label htmlFor="" className="col-form-label">
+                Contact Number
+              </label>
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                name="Contact Number"
+                className="form-control"
+                aria-describedby="passwordHelpInline"
+                onChange={handleValidity}
+              />
+            </div>
+          </div>
+          {errorData["Contact Number"] !== "" && (
+            <p
+              className="d-flex justify-content-center"
+              style={{ color: "red" }}
+            >
+              {errorData["Contact Number"]}
+            </p>
+          )}
+          <div className="justify-content-center row g-3 align-items-center">
+            <div className="col-3 text-center">
+              <label htmlFor="" className="col-form-label">
+                Email
+              </label>
+            </div>
+            <div className="col-auto">
+              <input
+                type="Email"
+                name="Email"
+                className="form-control"
+                aria-describedby="passwordHelpInline"
+                onChange={handleValidity}
+              />
+            </div>
+          </div>
+          {errorData["Email"] !== "" && (
+            <p
+              className="d-flex justify-content-center"
+              style={{ color: "red" }}
+            >
+              {errorData["Email"]}
+            </p>
+          )}
+          <div className="justify-content-center row g-3 align-items-center">
+            <div className="col-3 text-center">
+              <label htmlFor="" className="col-form-label">
+                Birthday
+              </label>
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                name="Birthday"
+                className="form-control"
+                aria-describedby="passwordHelpInline"
+                onChange={handleValidity}
+              />
+            </div>
+          </div>
+          {errorData["Birthday"] !== "" && (
+            <p
+              className="d-flex justify-content-center"
+              style={{ color: "red" }}
+            >
+              {errorData["Birthday"]}
+            </p>
+          )}
+          <div className="flex justify-content-center row g-3 align-items-center">
+            <div className="col-3 text-center">
+              <label htmlFor="" className="col-form-label">
+                Gender
+              </label>
+            </div>
+            <div className="col-5">
+              <select
+                className="w-100"
+                value={gender}
+                onChange={(e) => {
+                  setGender(e.target.value);
+                }}
+              >
+                <option value="select"> Select</option>
+                <option value="Male"> Male </option>
+                <option value="Female"> Female </option>
+              </select>
+            </div>
+          </div>
+          {genderError && (
+            <p
+              className="d-flex justify-content-center"
+              style={{ color: "red" }}
+            >
+              Select gender
             </p>
           )}
           <div className="btn-container d-flex justify-content-center ">
@@ -80,9 +230,6 @@ function AddPatient() {
             </button>
           </div>
         </div>
-        <Link to="" className="d-flex forgetPass justify-content-center">
-          Forget Password?
-        </Link>
       </div>
     </div>
   );
